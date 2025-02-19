5bb2c7a431225c2f6397301be519ec1c47350729
Francisco Aros <fco.aros.ch@gmail.com>
2025-02-19 17:04:07 -0300

    Relaciones en colecciones

--- 03-blog/src/content/config.ts
+++ 03-blog/src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
@@ -10,13 +10,24 @@ const blogCollection = defineCollection({
       image: image(),
 
       // relacion
-      author: z.string(),
+      // author: z.string(),
+      author: reference("author"),
 
       // relacion
       tags: z.array(z.string()),
     }),
 });
 
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+    }),
+});
+
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 };
